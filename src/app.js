9abfce3ed62a6d4b42cd7ac902ab7967ca0b8164
PauFugit <paulinefugit@gmail.com>
2022-09-01 00:17:03 -0400

    estatico

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let cardsArray = [];
+const Drawbtn = document.querySelector(".draw-btn");
+const Sortbtn = document.querySelector(".sort-btn");
+const card = document.querySelector(".card");
+const numb = document.querySelector(".numb");
+const input = document.querySelector("#amount-cards");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generateRandomNumber = () => {
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  const indexNumb = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumb];
+};
+
+const generateRandomSimb = () => {
+  const simb = ["hearts", "diamonds", "spades", "clubs"];
+  const indexSimb = Math.floor(Math.random() * simb.length);
+  return simb[indexSimb];
+};
+
+window.onload = () => {
+  card.classList.add(generateRandomSimb());
+  numb.innerHTML = generateRandomNumber();
 };
